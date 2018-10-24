fc6250c054009fbcca725b7698e1a07d2e0ecc17
Vasylyna <vasylyna.kit@gmail.com>
2018-10-24 23:01:00 +0300

    ..saved by partners-editor page

--- src/app/partners-editor/partners-editor.component.ts
+++ src/app/partners-editor/partners-editor.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {PartnersService} from '../shared/partners.service';
+import {Partners} from '../shared/partners.model';
 
 @Component({
   selector: 'app-partners-editor',
@@ -6,10 +9,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./partners-editor.component.css']
 })
 export class PartnersEditorComponent implements OnInit {
+partners:Partners;
 
-  constructor() { }
+  constructor(private currentRoute:ActivatedRoute, private service: PartnersService) { }
 
   ngOnInit() {
+  let id:number = +this.currentRoute.snapshot.paramMap.get('id');
+  this.partners = this.service.getById(id);
   }
 
 }
